19a8d74b1aecd9197356f970a6d92200cfb1ef51
Tobias Kokesch <tobias@binary-hype.com>
2024-08-08 11:31:06 +0200

    🔧 fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-
-const port = 5173;
-const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
 
 export default defineConfig({
     plugins: [
@@ -12,12 +9,12 @@ export default defineConfig({
         }),
     ],
     server: {
-        // respond to all network requests
-        host: '0.0.0.0',
-        port: port,
+        host: true,
         strictPort: true,
-        // Defines the origin of the generated asset URLs during development,
-        // this will also be used for the public/hot file (Vite devserver URL)
-        origin: origin
-    }
+        port: 5173,
+        hmr: {
+            host: process.env.DDEV_HOSTNAME,
+            protocol: 'wss'
+        },
+    },
 });
